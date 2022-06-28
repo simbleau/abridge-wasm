da60e4d3e6041aa7f9b493c3caedc92dcdbe8f3f
Jieiku <106644+Jieiku@users.noreply.github.com>
2022-06-27 21:16:19 -0700

    Apply Class to Root element instead of Body

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,14 @@ use wasm_bindgen::prelude::*;
 pub fn set_theme1() {
     let window = web_sys::window().expect("no global `window` exists");
     let document = window.document().expect("should have a document on window");
-    let body = document.body().expect("document should have a body");
-    body.set_class_name("theme1");
+    let element = document.document_element().expect("document should have a root element");
+    element.set_class_name("theme1");
 }
 
 #[wasm_bindgen]
 pub fn set_theme2() {
     let window = web_sys::window().expect("no global `window` exists");
     let document = window.document().expect("should have a document on window");
-    let body = document.body().expect("document should have a body");
-    body.set_class_name("theme2");
+    let element = document.document_element().expect("document should have a root element");
+    element.set_class_name("theme2");
 }
