4d8b8530ed4e6ad046d3e7edcf86af9257cd7b8e
Spencer Imbleau <spencer@imbleau.com>
2022-06-29 04:34:55 -0400

    Optimized WASM for theme-switch

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
 // Anything included here will be exported by WASM bindgen.
 // Even non-public modules. Uncomment a line to build the WASM.
 
-// mod theme_switch;
+pub mod theme_switch;
--- src/theme_switch.rs
+++ src/theme_switch.rs
@@ -1,21 +1,26 @@
 use wasm_bindgen::prelude::*;
+use web_sys::Element;
+
+pub fn root() -> Element {
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    document
+        .document_element()
+        .expect("document should have a root element")
+}
+
+pub fn set_class(element: web_sys::Element, name: &str) {
+    element.set_class_name(&name)
+}
 
 #[wasm_bindgen]
 pub fn set_theme1() {
-    let window = web_sys::window().expect("no global `window` exists");
-    let document = window.document().expect("should have a document on window");
-    let element = document
-        .document_element()
-        .expect("document should have a root element");
-    element.set_class_name("theme1");
+    set_class(root(), "theme1");
 }
 
 #[wasm_bindgen]
 pub fn set_theme2() {
-    let window = web_sys::window().expect("no global `window` exists");
-    let document = window.document().expect("should have a document on window");
-    let element = document
-        .document_element()
-        .expect("document should have a root element");
-    element.set_class_name("theme2");
+    set_class(root(), "theme2");
 }
+
+pub fn hook() {}
